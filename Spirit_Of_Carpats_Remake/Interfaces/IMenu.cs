4a209c2b8d677527c4b76bd0c798879e8ca7150e
matviu08 <149872744+matviu08@users.noreply.github.com>
2026-03-07 14:44:11 +0200

    create Imenu
    test

---
+++ Spirit_Of_Carpats_Remake/Interfaces/IMenu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spirit_Of_Carpats_Remake.Interfaces
+{
+    internal interface IMenu
+    {
+    }
+}

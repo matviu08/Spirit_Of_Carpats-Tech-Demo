22fbb458d1bbc5d16d2303553b470ad70f798ea7
matviu08 <149872744+matviu08@users.noreply.github.com>
2026-03-08 11:37:25 +0200

    просто зробив метод

--- Spirit_Of_Carpats_Remake/Program.cs
+++ Spirit_Of_Carpats_Remake/Program.cs
@@ -10,7 +10,6 @@ class Program
 {
     static GameState currentState = GameState.MainMenu;
     static GameState targetState = GameState.MainMenu;
-
     static float fadeAlpha = 0f;
     static bool isTransitioning = false;
     static bool isClosing = false;
@@ -58,36 +57,19 @@ class Program
                     }
                 }
 
-                if (isTransitioning)
-                {
-                    fadeAlpha += 5f; 
-                    if (fadeAlpha >= 255)
-                    {
-                        fadeAlpha = 255;
-                        if (targetState == GameState.Closing || currentState == GameState.Closing)
-                        {
-                            isClosing = true; 
-                        }
-                        else
-                        {
-                            currentState = targetState; 
-                            isTransitioning = false;
-                        }
-                    }
-                }
-                else if (fadeAlpha > 0)
-                {
-                    fadeAlpha -= 5f; 
-                    if (fadeAlpha < 0) fadeAlpha = 0;
-                }
+                HandleFadeLogic();
 
                 BeginDrawing();
                 ClearBackground(Color.Black);
 
                 if (currentState == GameState.MainMenu)
+                {
                     DrawTexture(menuBackgroundTexture, 0, 0, Color.White);
+                }
                 else if (currentState == GameState.Settings)
+                {
                     DrawTexture(optionBackgroundTexture, 0, 0, Color.White);
+                }
 
                 mainMenu.Draw(currentState);
 
@@ -109,5 +91,28 @@ class Program
         UnloadMusicStream(ambientMusic);
         CloseAudioDevice();
         CloseWindow();
+    }
+
+    static void HandleFadeLogic()
+    {
+        if (isTransitioning)
+        {
+            fadeAlpha += 5f;
+            if (fadeAlpha >= 255)
+            {
+                fadeAlpha = 255;
+                if (targetState == GameState.Closing) isClosing = true;
+                else
+                {
+                    currentState = targetState;
+                    isTransitioning = false;
+                }
+            }
+        }
+        else if (fadeAlpha > 0)
+        {
+            fadeAlpha -= 5f;
+            if (fadeAlpha < 0) fadeAlpha = 0;
+        }
     }
 }

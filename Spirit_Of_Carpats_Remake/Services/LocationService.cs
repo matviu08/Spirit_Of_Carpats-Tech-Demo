9b4ede8210a8c30179f83c073e8d0ed0fb66c5cb
BobrickCS22 <132747159+BobrickCS22@users.noreply.github.com>
2026-03-08 19:27:42 +0200

    Update LocationService.cs

--- Spirit_Of_Carpats_Remake/Services/LocationService.cs
+++ Spirit_Of_Carpats_Remake/Services/LocationService.cs
@@ -4,6 +4,7 @@ using Spirit_Of_Carpats_Remake.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using static Raylib_cs.Raylib;
@@ -13,13 +14,32 @@ namespace Spirit_Of_Carpats_Remake.Services
     public class LocationService : ILocationService
     {
         private Texture2D _forestBackground;
+        
         public LocationService()
         {
             _forestBackground = LoadTexture(".\\Resurses\\Img\\WoodBackground.png");
         }
         public void Draw(GameState state)
         {
-            DrawTexture(_forestBackground, 0, 0, Color.White);
+            float scaleX = (float)GetScreenWidth() / _forestBackground.Width;
+            float scaleY = (float)GetScreenHeight() / _forestBackground.Height;
+
+            float scale = Math.Max(scaleX, scaleY);
+
+            float width = _forestBackground.Width * scale;
+            float height = _forestBackground.Height * scale;
+
+            float posX = (GetScreenWidth() - width) / 2;
+            float posY = (GetScreenHeight() - height) / 2;
+
+            DrawTexturePro(
+                _forestBackground,
+                new Rectangle(0, 0, _forestBackground.Width, _forestBackground.Height),
+                new Rectangle(posX, posY, width, height),
+                new Vector2(0, 0),
+                0,
+                Color.White
+            );
         }
 
         public void Update(ref GameState state)
@@ -32,6 +52,7 @@ namespace Spirit_Of_Carpats_Remake.Services
         public void Unload()
         {
             UnloadTexture(_forestBackground);
+
         }
     }
 }

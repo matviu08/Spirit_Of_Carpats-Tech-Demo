d48eb4b478fd044c6f9beebc322424e56ebe0382
forking2 <104146383+forking2@users.noreply.github.com>
2026-03-08 10:52:35 +0200

    AddedSaveLayer

---
+++ BLL/Models/Save.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Models
+{
+    public class Save
+    {
+        public int PlayerHp { get; set; }
+        public int ChapterName { get; set; }
+        public string Location { get; set; }
+        public int PosX { get; set; }
+        public int PosY { get; set; }
+    }
+}

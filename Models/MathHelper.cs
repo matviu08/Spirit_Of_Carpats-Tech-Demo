719f5d72d428e5edb918f07b09b3662ea0526521
forking2 <104146383+forking2@users.noreply.github.com>
2026-06-11 23:35:27 +0300

    Revert "Light system"
    This reverts commit 9a653392f3aae1ccd83f78936e470c60a6589e5f.

--- Models/MathHelper.cs
+++
@@ -1,8 +0,0 @@
-namespace Spirit_Of_Carpats_Remake.Models
-{
-    internal static class MathHelper
-    {
-        public static float Lerp(float a, float b, float t)
-            => a + (b - a) * Math.Clamp(t, 0f, 1f);
-    }
-}
---
+++ SaveLayer/Models/Save.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spirit_Of_Carpats_Remake.Models;
+namespace BLL.Models
+{
+    [Serializable]
+    public class Save
+    {
+        public int PlayerHp { get; set; }
+        public Chapter ChapterNum { get; set; }
+        public string LocationName { get; set; }
+        public int PosX { get; set; }
+        public int PosY { get; set; }
+    }
+}
